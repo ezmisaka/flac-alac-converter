4b7124ab7231da615c55fa33a62d1e23b40785ea
ezmisaka <164748659+ezmisaka@users.noreply.github.com>
2024-12-19 13:53:21 +0800

    Update flac-alac converter.bat

--- flac-alac converter.bat
+++ flac-alac converter.bat
@@ -1,6 +1,5 @@
 @echo off
 mkdir ALAC
 for %%f in (*.flac) do (
-  ffmpeg -i "%%f" -acodec alac -vcodec copy "%%~nf.m4a"
-  move "%%~nf.m4a" "ALAC\%%~nf.m4a"
+  ffmpeg -i "%%f" -acodec alac -vcodec copy "ALAC\%%~nf.m4a"
 )
